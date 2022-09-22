d53f45750a1d1a1644e6105dcbc8bb82ffd21b17
sandeepkr1084@ <sandeepkr1084@gmail.com>
2022-09-22 14:20:06 +0530

    fixed swagger

--- src/main/java/com/tweetapp/api/config/SwaggerConfiguration.java
+++ src/main/java/com/tweetapp/api/config/SwaggerConfiguration.java
@@ -19,7 +19,7 @@ class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.regex("/api/"))
+                .paths(PathSelectors.regex("/api.*"))
                 .build();
     }
 
